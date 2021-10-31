24202e4285e4f5c7a164d36890df4488db2f3033
Crispin R <crispinr003@gmail.com>
2021-10-31 17:24:02 +0530

    Add endless loop with case

--- app.java
+++ app.java
@@ -1,3 +1,27 @@
+import java.util.Scanner;
+
 public class app {
 
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("~Mr. Attendance~");
+        while (true) {
+            System.out.println("Select a login \tS - Student \tF - Faculty");
+            char key;
+
+            key = s.next().charAt(0);
+            switch (key) {
+            case 'S':
+                System.out.println("Student!");
+                break;
+            case 'F':
+                System.out.println("Faculty!");
+                break;
+            default:
+                break;
+            }
+            break;
+        }
+        s.close();
+    }
 }

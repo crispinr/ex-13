877afa388168a7d2bf8fa0a92381710a37cfadcb
Crispin R <crispinr003@gmail.com>
2021-11-02 00:45:49 +0530

    Add interface utilities

--- app.java
+++ app.java
@@ -1,27 +1,49 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.regex.*;
+
+import javax.swing.text.Utilities;
+
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-public class app {
+interface utilities {
 
-    public static Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$",
-            Pattern.CASE_INSENSITIVE);
+    void date();
 
-    public static boolean validate(String emailStr) {
+    boolean validate(String emailStr);
+}
+
+class util implements utilities {
+
+    public void date() {
+        String date;
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd/MM/yy");
+        LocalDateTime now = LocalDateTime.now();
+        date = dateFormat.format(now);
+        System.out.println("Attendance List for " + date + ": ");
+    }
+
+    public boolean validate(String emailStr) {
+        Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$",
+                Pattern.CASE_INSENSITIVE);
         Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(emailStr);
         return matcher.find();
     }
+}
+
+public class app {
 
     public static void main(String args[]) {
 
         // variables
         String email, name;
         char key;
+
+        util u = new util();
 
         Scanner s = new Scanner(System.in);
         System.out.println("~Mr. Attendance~");
@@ -33,13 +55,9 @@ public class app {
                 System.out.println("Student!");
                 System.out.print("Welcome to student login \nEnter your email: ");
                 email = s.next();
-                if (validate(email)) {
+                if (u.validate(email)) {
                     System.out.println("Nice");
-                    DateTimeFormatter dateTimeFormat = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-                    LocalDateTime now = LocalDateTime.now();
-                    String dateAndTime = dateTimeFormat.format(now);
-                    System.out.println(dateAndTime);
-                    System.out.println("Attendance List for " + dateAndTime + ": ");
+                    u.date();
                     System.out.print("Enter the total class strength: ");
                     int tot = s.nextInt();
                     ArrayList<String> studList = new ArrayList<String>(tot);
@@ -73,13 +91,9 @@ public class app {
                 System.out.println("Faculty!");
                 System.out.print("Welcome to faculty login \nEnter your email: ");
                 email = s.next();
-                if (validate(email)) {
+                if (u.validate(email)) {
                     System.out.println("Nice");
-                    DateTimeFormatter dateTimeFormat = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-                    LocalDateTime now = LocalDateTime.now();
-                    String dateAndTime = dateTimeFormat.format(now);
-                    System.out.println(dateAndTime);
-                    System.out.println("Attendance List for " + dateAndTime + ": ");
+                    u.date();
                     System.out.print("Enter the total class strength: ");
                     int tot = s.nextInt();
                     ArrayList<String> studList = new ArrayList<String>(tot);
